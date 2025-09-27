9b410a64702ed018a627b394361f28767a4736e9
Jordan Legg <149483255+404missinglink@users.noreply.github.com>
2025-09-27 17:55:04 +0200

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Indicate that these packages should not be bundled by webpack
-  experimental: {
-    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
-  },
+  serverExternalPackages: ['sharp', 'onnxruntime-node'],
 };
 
 export default nextConfig;
