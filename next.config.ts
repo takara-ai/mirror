53d08287291dbe3f2c7a66283881946dd91f83fa
Jordan Legg <149483255+404missinglink@users.noreply.github.com>
2025-09-27 16:39:45 +0200

    fix: module type

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@xenova/transformers'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@xenova/transformers');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
