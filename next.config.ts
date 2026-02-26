6da6ce997f197db02bfce7edfb6f445d8c7dd4ab
Jordan Legg <149483255+404missinglink@users.noreply.github.com>
2026-02-26 17:03:24 +0000

    fix: hostname

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.blob.vercel-storage.com",
+        hostname: "**.public.blob.vercel-storage.com",
         pathname: "/**",
       },
     ],
